5bdd2f5a15733433f892458fa889364705e19b46
vkotsiuba99 <92172262+vkotsiuba99@users.noreply.github.com>
2022-05-11 15:46:08 +0300

    Ready model

--- main.py
+++ main.py
@@ -22,22 +22,13 @@ training_labels = training_labels[:20000]
 testing_images = testing_images[:4000]
 testing_labels = testing_labels[:4000]
 
-model = models.Sequential()
-model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3)))
-model.add(layers.MaxPooling2D((2,2)))
-model.add(layers.Conv2D(64, (3,3), activation='relu'))
-model.add(layers.MaxPooling2D((2,2)))
-model.add(layers.Conv2D(64, (3,3), activation='relu'))
-model.add(layers.Flatten())
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(10, activation='softmax'))
+model = models.load_model('image_classifier.model')
 
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+img = cv.imread('image.jpg')
+img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 
-model.fit(training_images, training_labels, epochs=10, validation_data=(testing_images, testing_labels))
+plt.imshow(img, cmap=plt.cm.binary)
 
-loss, accuracy = model.evaluate(testing_images, testing_labels)
-print(f"Loss: {loss}")
-print(f"Accuracy {accuracy}")
-
-model.save('image_classifier.model')
+prediction = model.predict(np.array([img]) / 255)
+index = np.argmax(prediction)
+print(f'Prediction is {class_name[index]}')
